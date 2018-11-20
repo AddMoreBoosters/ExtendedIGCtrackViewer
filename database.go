2d733e98017d7477533fb64f7a2d959cdc8e8a1d
Herman <hermantd@stud.ntnu.no>
2018-11-20 18:01:52 +0100

    Added GetIndexes

--- database.go
+++ database.go
@@ -15,8 +15,8 @@ func (db *TrackDB) Init () {
 	defer session.Close()
 }
 
-//	Adds a track to the database
-func (db *TrackDB) Add (track Track) {
+//	Adds a track to the database, and returns its new id
+func (db *TrackDB) Add (track Track) string {
 	session, err := mgo.Dial(db.DatabaseURL)
 	if err != nil {
 		panic(err)
@@ -27,6 +27,12 @@ func (db *TrackDB) Add (track Track) {
 	if err != nil {
 		fmt.Printf("Error in Insert(): %v", err.Error())
 	}
+
+	err = session.DB(db.DatabaseName).C("Placeholder").Find(bson.M{"track_src_url": track.TrackSourceURL}).One(&track)
+	if err != nil {
+		fmt.Printf("Error in Find().One(): %v", err.Error())
+	}
+	return track._id
 }
 
 //	Gets a track from the database
@@ -38,13 +44,35 @@ func (db *TrackDB) Get (keyID string) (Track, bool) {
 	defer session.Close()
 
 	track := Track{}
-	err = session.DB(db.DatabaseName).C("Placeholder").Find(bson.M{"trackid": keyID}).One(&track)
+	err = session.DB(db.DatabaseName).C("Placeholder").Find(bson.M{"_id": keyID}).One(&track)
 	if err != nil {
 		fmt.Printf("Error in Find().One(): %v", err.Error())
 		return track, false
 	}
 
 	return track, true
+}
+
+//	Gets all indexes from the database
+func (db *TrackDB) GetIndexes () []string {
+	session, err := mgo.Dial(db.DatabaseURL)
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	var docs []Track
+	err = session.DB(db.DatabaseName).C("Placeholder").Find().All(&docs)
+	if err != nil {
+		fmt.Printf("Error in Find().All(): %v", err.Error())
+		return nil
+	}
+
+	var indexes []string
+	for _, doc := range docs {
+		indexes = append(indexes, doc._id)
+	}
+	return indexes
 }
 
 //	Counts the tracks in the database
