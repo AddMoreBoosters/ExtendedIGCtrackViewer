0a2fc873f1a8f7b65cf46cef0c3713ec618bab7d
Herman <hermantd@stud.ntnu.no>
2018-11-20 16:52:01 +0100

    Added database functionality

--- database.go
+++ database.go
@@ -2,25 +2,63 @@ package main
 
 import (
 	"github.com/globalsign/mgo"
+	"github.com/globalsign/mgo/bson"
+	"fmt"
 )
 
-type TrackDB struct {
-	DatabaseURL string
-	DatabaseName string
-}
-
+//	Initializes the database
 func (db *TrackDB) Init () {
-
+	session, err := mgo.Dial(db.DatabaseURL)
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
 }
 
+//	Adds a track to the database
 func (db *TrackDB) Add (track Track) {
-	
+	session, err := mgo.Dial(db.DatabaseURL)
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	err = session.DB(db.DatabaseName).C("Placeholder").Insert(track)
+	if err != nil {
+		fmt.Printf("Error in Insert(): %v", err.Error())
+	}
 }
 
-func (db *TrackDB) Get (keyID string) {
-	
+//	Gets a track from the database
+func (db *TrackDB) Get (keyID string) (Track, bool) {
+	session, err := mgo.Dial(db.DatabaseURL)
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	track := Track{}
+	err = session.DB(db.DatabaseName).C("Placeholder").Find(bson.M{"trackid": keyID}).One(&track)
+	if err != nil {
+		fmt.Printf("Error in Find().One(): %v", err.Error())
+		return track, false
+	}
+
+	return track, true
 }
 
+//	Counts the tracks in the database
 func (db *TrackDB) Count () int {
-	
+	session, err := mgo.Dial(db.DatabaseURL)
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	count, err := session.DB(db.DatabaseName).C("Placeholder").Count()
+	if err != nil {
+		fmt.Printf("Error in Count(): %v", err.Error())
+		return -1
+	}
+	return count
 }
--- structs.go
+++ structs.go
@@ -1,5 +1,17 @@
 package main 
 
+type TrackDB struct {
+	DatabaseURL string
+	DatabaseName string
+	CollectionName string
+}
+
+type ApiMetadata struct {
+	Uptime  string
+	Info    string
+	Version string
+}
+
 type Track struct {
 	Hdate       	time.Time 	`json:"H_date"`
 	Pilot       	string    	`json:"pilot"`
